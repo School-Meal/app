ae1c37d0f062cc79096015667421f1c6b7ac5539
jinhyeon-dev <jinhyeon.dev@gmail.com>
2024-08-14 01:31:52 +0900

    build: flutter_secure_storage

--- lib/screen/auth/signin_screen.dart
+++ lib/screen/auth/signin_screen.dart
@@ -1,4 +1,6 @@
+import 'dart:convert';
 import 'package:flutter/material.dart';
+import 'package:http/http.dart' as http;
 import 'package:school_meal/screen/auth/signup_screen.dart';
 import 'package:school_meal/screen/navigator.dart';
 
@@ -12,6 +14,65 @@ class SignInScreen extends StatefulWidget {
 class _SignInScreenState extends State<SignInScreen> {
   final TextEditingController _emailController = TextEditingController();
   final TextEditingController _passwordController = TextEditingController();
+
+  Future<void> _signIn() async {
+    final String email = _emailController.text;
+    final String password = _passwordController.text;
+
+    if (email.isEmpty || password.isEmpty) {
+      _showErrorDialog('이메일과 비밀번호를 입력해주세요.');
+      return;
+    }
+
+    final url = Uri.parse('http://52.78.20.150/auth/signin');
+
+    try {
+      final response = await http.post(
+        url,
+        headers: {'Content-Type': 'application/json'},
+        body: json.encode({'email': email, 'password': password}),
+      );
+
+      if (response.statusCode == 201) {
+        // 서버로부터 accessToken과 refreshToken을 받아서 처리
+        final responseData = json.decode(response.body);
+        final accessToken = responseData['accessToken'];
+        final refreshToken = responseData['refreshToken'];
+
+        // 토큰을 안전한 곳에 저장 (예: SharedPreferences, SecureStorage 등)
+        // await SecureStorage.write(key: 'accessToken', value: accessToken);
+        // await SecureStorage.write(key: 'refreshToken', value: refreshToken);
+
+        // MainNavigator로 이동
+        Navigator.pushReplacement(
+          context,
+          MaterialPageRoute(builder: (context) => const MainNavigator()),
+        );
+      } else {
+        _showErrorDialog('로그인에 실패했습니다. 다시 시도해주세요.');
+      }
+    } catch (error) {
+      _showErrorDialog('오류가 발생했습니다. 인터넷 연결을 확인해주세요.');
+    }
+  }
+
+  void _showErrorDialog(String message) {
+    showDialog(
+      context: context,
+      builder: (ctx) => AlertDialog(
+        title: const Text('오류'),
+        content: Text(message),
+        actions: [
+          TextButton(
+            onPressed: () {
+              Navigator.of(ctx).pop();
+            },
+            child: const Text('확인'),
+          ),
+        ],
+      ),
+    );
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -49,13 +110,7 @@ class _SignInScreenState extends State<SignInScreen> {
                   _buildTextField(_passwordController, '비밀번호', Icons.lock,
                       isPassword: true),
                   const SizedBox(height: 30),
-                  _buildButton("로그인", () {
-                    Navigator.push(
-                      context,
-                      MaterialPageRoute(
-                          builder: (context) => const MainNavigator()),
-                    );
-                  }),
+                  _buildButton("로그인", _signIn),
                   const SizedBox(height: 15),
                   _buildButton("홈화면", () {
                     Navigator.push(
